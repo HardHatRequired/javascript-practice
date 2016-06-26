7454cc8c8b0ee28b990a198e419fb464279e410f
HardHatArea <jon.hoopes@gmail.com>
2016-06-25 18:22:03 -0600

    fixed

---
+++ app/app.js
@@ -0,0 +1,10 @@
+var firstName = "Jonathan";
+// var lastName = "Hoopes";
+
+
+function giveName (lastName) {
+  return = firstName + " " + lastName;
+}
+
+var fullName = giveName('Hoopes');
+console.log(fullName);
